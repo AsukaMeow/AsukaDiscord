774ccaf15cb60f48fa6d216c118be8fe0d9219f1
clooooode <jackey8616@gmail.com>
2019-08-11 22:24:49 +0800

    Init commit

---
+++ src/main/java/at/meowww/asuka_discord/AsukaDiscord.java
@@ -0,0 +1,15 @@
+package at.meowww.asuka_discord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AsukaDiscord extends JavaPlugin {
+
+    @Override
+    public void onEnable () {
+    }
+
+
+    @Override
+    public void onDisable() {}
+
+}
